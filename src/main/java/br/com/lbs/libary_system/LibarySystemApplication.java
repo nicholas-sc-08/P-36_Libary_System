7f35a0b72dff6df428fb3da32a19e9a061043467
nicholas-sc-08 <nicholassc.2008@gmail.com>
2026-01-11 17:18:27 -0300

    initial commit

---
+++ src/main/java/br/com/lbs/libary_system/LibarySystemApplication.java
@@ -0,0 +1,13 @@
+package br.com.lbs.libary_system;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LibarySystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LibarySystemApplication.class, args);
+	}
+
+}
